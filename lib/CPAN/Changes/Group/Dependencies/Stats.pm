a91f60ac11e1b2a56f844cc0cd2343ae874100c0
Kent Fredric <kentfredric@gmail.com>
2017-03-04 21:44:51 +1300

    Build results of 21354ae (on master)

--- lib/CPAN/Changes/Group/Dependencies/Stats.pm
+++ lib/CPAN/Changes/Group/Dependencies/Stats.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package CPAN::Changes::Group::Dependencies::Stats;
 
-our $VERSION = '0.002007';
+our $VERSION = '0.002008';
 
 # ABSTRACT: Create a Dependencies::Stats section detailing summarized differences
 
@@ -209,7 +209,7 @@ CPAN::Changes::Group::Dependencies::Stats - Create a Dependencies::Stats section
 
 =head1 VERSION
 
-version 0.002007
+version 0.002008
 
 =head1 SYNOPSIS
 
@@ -302,7 +302,7 @@ Kent Fredric <kentnl@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2015 by Kent Fredric <kentfredric@gmail.com>.
+This software is copyright (c) 2017 by Kent Fredric <kentfredric@gmail.com>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
