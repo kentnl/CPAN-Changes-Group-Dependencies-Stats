e12f70cec4b241f78a3b693e89b504b89a3762ee
Kent Fredric <kentfredric@gmail.com>
2017-03-04 21:45:26 +1300

    Build results of 7abbc92 (on master)

--- lib/CPAN/Changes/Group/Dependencies/Stats.pm
+++ lib/CPAN/Changes/Group/Dependencies/Stats.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package CPAN::Changes::Group::Dependencies::Stats;
 
-our $VERSION = '0.002008';
+our $VERSION = '0.002009';
 
 # ABSTRACT: Create a Dependencies::Stats section detailing summarized differences
 
@@ -209,7 +209,7 @@ CPAN::Changes::Group::Dependencies::Stats - Create a Dependencies::Stats section
 
 =head1 VERSION
 
-version 0.002008
+version 0.002009
 
 =head1 SYNOPSIS
 
